4af671125e097fcf1e805372e2be6a9502e4a0f8
Raphael Krut-Landau <raphael.kl@gmail.com>
2017-02-18 15:43:13 -0500

    hasty commit without summary

--- new/final.sql
+++ new/final.sql
@@ -3,47 +3,57 @@ set sql_big_selects=1;
 -- I use the oldfashioned abbreviation 'et ux' to mean 'and their spouses'
 
 select
-concat(Cemetery, ' ', Section, ' ', Subsection, ' ', Plot2) as plot,
-AAFirstName as first_name,
-AASurname as last_name,
-group_concat(distinct concat(spouse.relation_fullname)) as spouses,
-concat(
+concat(Cemetery, ' ', Section, ' ', Subsection, ' ', Plot2) as plot
+,AAFirstName as first_name
+,AASurname as last_name
+,group_concat(distinct concat(spouse.relation_fullname)) as spouses
+,concat(
     group_concat(distinct concat(parent.relation_fullname))
     -- ,',',group_concat(distinct concat(spouse_of_parent.relation_fullname)), ','
-) as parents_et_ux,
-concat(
+) as parents_et_ux
+,concat(
     group_concat(distinct concat(nibling.relation_fullname))
     -- ,',',group_concat(distinct concat(spouse_of_nibling.relation_fullname))
-) as aunts_and_uncles_et_ux,
-concat(
-    group_concat(distinct concat(sibling.relation_fullname))
-    -- ,',',group_concat(distinct concat(spouse_of_sibling.relation_fullname))
-) as siblings_et_ux,
-concat(
+) as aunts_and_uncles_et_ux
+-- ,concat(
+--     group_concat(distinct concat(sibling.relation_fullname))
+--     -- ,',',group_concat(distinct concat(spouse_of_sibling.relation_fullname))
+-- ) as siblings_et_ux
+,concat(
     group_concat(distinct concat(grandchild.relation_fullname))
     -- ,',',group_concat(distinct concat(spouse_of_grandchild.relation_fullname))
 ) as grandchildren_et_ux
--- ,
--- concat(
+-- ,concat(
 --     group_concat(distinct concat(grandnibling.relation_fullname))
 --     -- ,',',group_concat(distinct concat(spouse_of_grandnibling.relation_fullname)), ','
 -- ) as grandnieces_and_grandnephews_et_ux,
--- concat(
---     group_concat(distinct concat(firstcousin.relation_fullname))
---     ,',',group_concat(distinct concat(spouse_of_firstcousin.relation_fullname))
--- ) as first_cousins_et_ux
+,concat(
+    group_concat(distinct concat(firstcousin.relation_fullname))
+    -- ,',',group_concat(distinct concat(spouse_of_firstcousin.relation_fullname))
+) as first_cousins_et_ux
 from
 JewishMeNames p
 join is_child_of parent
-join is_sibling_of sibling
+on p.id = parent.person_id
+-- join is_sibling_of sibling
+
 join has_been_married_to spouse
+on p.id = spouse.person_id
+
 join is_grandparent_of grandchild
--- join is_grandparent_of_spouse_of spouse_of_grandchild
+on p.id = grandchild.person_id
+
+-- -- join is_grandparent_of_spouse_of spouse_of_grandchild
+
 join is_auntuncle_of nibling
--- join is_auntuncle_of_spouse_of spouse_of_nibling
+on p.id = nibling.person_id
+
+-- -- join is_auntuncle_of_spouse_of spouse_of_nibling
+
 join is_firstcousin_of firstcousin
-join is_firstcousin_of_spouse_of spouse_of_firstcousin
-where p.AASurname = "Gleckman"
-AND p.AAFirstName like "Frances%"
-group by first_name, last_name
--- LIMIT 99999999
+on p.id = firstcousin.person_id
+
+-- join is_firstcousin_of_spouse_of spouse_of_firstcousin
+where True
+-- group by first_name, last_name
+LIMIT 99999999
