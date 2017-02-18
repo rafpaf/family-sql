86da83f0b56d488287e5c26694ab02b9da957c7d
Raphael Krut-Landau <raphael.kl@gmail.com>
2017-02-18 18:04:24 -0500

    hasty commit without summary

--- new/final.sql
+++ new/final.sql
@@ -47,32 +47,32 @@ Cemetery
 from
 JewishMeNames p
 
-left join is_child_of parent
+join is_child_of parent
 on p.id = parent.person_id
 
-left join is_child_of child
+join is_child_of child
 on p.id = parent.relation_id
 
-left join is_sibling_of sibling
+join is_sibling_of sibling
 on p.id = sibling.person_id
 
-left join has_been_married_to spouse
+join has_been_married_to spouse
 on p.id = spouse.person_id
 
-left join is_grandparent_of grandchild
+join is_grandparent_of grandchild
 on p.id = grandchild.person_id
 
 -- -- join is_grandparent_of_spouse_of spouse_of_grandchild
 
-left join is_niecenephew_of nibling
+join is_niecenephew_of nibling
 on p.id = nibling.relation_id
 
 -- -- join is_auntuncle_of_spouse_of spouse_of_nibling
 
-left join is_firstcousin_of firstcousin
+join is_firstcousin_of firstcousin
 on p.id = firstcousin.person_id
 
-left join is_firstcousin_of_spouse_of spouse_of_firstcousin
+join is_firstcousin_of_spouse_of spouse_of_firstcousin
 on p.id = spouse_of_firstcousin.person_id
 
 where p.sinai
