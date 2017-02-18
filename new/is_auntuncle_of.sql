86e76727e2836492cd1bdc39fd64db9afd36b9cb
Raphael Krut-Landau <raphael.kl@gmail.com>
2017-02-18 14:59:39 -0500

    hasty commit without summary

--- new/is_auntuncle_of.sql
+++ new/is_auntuncle_of.sql
@@ -3,13 +3,15 @@ set sql_big_selects=1;
 -- if x is_child_of y and z is_child_of y then x is_sibling_of z
 -- this includes half siblings
 
--- insert into is_auntuncle_of (person_id, person_fullname, relation_id, relation_fullname)
+create table if not exists is_auntuncle_of like is_child_of;
+
+insert into is_auntuncle_of (person_id, person_fullname, relation_id, relation_fullname)
 select
 
 distinct
-p.PersonId as person_id,
+p.id as person_id,
 p.fullname as person_fullname,
-p2.PersonId as relation_id,
+p2.id as relation_id,
 p2.fullname as relation_fullname
 
 from JewishMeNames as p
@@ -18,11 +20,10 @@ join is_child_of as rc
 join JewishMeNames as p2
 
 where
-    p.PersonId = rs.person_id
+    p.id = rs.person_id
 AND rs.relation_id = rc.person_id
-AND rc.relation_id = p2.PersonId
+AND rc.relation_id = p2.id
 AND rc.person_id <> rc.relation_id
-AND p2.AASurname in ("Krut-Landau", "Chase")
 LIMIT 0, 999999
 
 \G;
