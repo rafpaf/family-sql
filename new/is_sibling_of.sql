3856c423b7bd66d421f51fedcf6e71222b9fe4ee
Raphael Krut-Landau <raphael.kl@gmail.com>
2017-02-18 12:17:22 -0500

    hasty commit without summary

--- new/is_sibling_of.sql
+++ new/is_sibling_of.sql
@@ -5,17 +5,24 @@ set sql_big_selects=1;
 
 -- insert into relations (person_id, person_fullname, relation_type, relation_id, relation_fullname)
 select
+
+-- ignore these
 p.PersonId as person_id,
 p.fullname as person_fullname,
 'is_sibling_of' as relation_type,
 sib.PersonId as relation_id,
 sib.fullname as relation_fullname
+
 from JewishMeNames as p
 join relations as r
 on p.PersonId = r.person_id
 join relations as r2
-on (r.person_id = r2.relation_id
+on (r.relation_id = r2.relation_id
     and r.relation_type = 'is_child_of'
-    and r2.relation_type = 'is_parent_of')
-where relation_id <> 0
+    and r2.relation_type = 'is_child_of')
+join JewishMeNames as sib
+on sib.PersonId = r2.relation_id
+where r.relation_id <> 0
+and p.AASurname in ("Gleckman", "Tarr")
+and p.PersonId <> sib.PersonId
 \G;
