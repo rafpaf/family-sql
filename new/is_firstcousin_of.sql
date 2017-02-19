093f8fa6a6983e182a601a27274c863ee255bc78
Raphael Krut-Landau <raphael.kl@gmail.com>
2017-02-19 09:48:55 -0500

    hasty commit without summary

--- new/is_firstcousin_of.sql
+++ new/is_firstcousin_of.sql
@@ -1,23 +1,19 @@
 set sql_big_selects=1;
 
-drop table is_firstcousin_of;
-
-create table if not exists is_firstcousin_of like is_child_of;
-
-insert into is_firstcousin_of (person_id, person_fullname, relation_id, relation_fullname)
+-- drop table is_firstcousin_of;
+-- 
+-- create table if not exists is_firstcousin_of like is_child_of;
+-- 
+-- insert into is_firstcousin_of (person_id, person_fullname, relation_id, relation_fullname)
 select
 distinct
-p.id as person_id,
-p.fullname as person_fullname,
-p2.id as relation_id,
-p2.fullname as relation_fullname
-from JewishMeNames as p
-join is_niecenephew_of as nn
+nn.person_id as person_id,
+nn.person_fullname as person_fullname,
+c.person_id as relation_id,
+c.person_fullname as relation_fullname
+from is_niecenephew_of as nn
 join is_child_of as c
-join JewishMeNames as p2
-where
-p.id = nn.person_id
-AND nn.relation_id = c.relation_id
-AND c.person_id = p2.id
+where nn.relation_id = c.relation_id
+AND nn.person_fullname like "Frances%Gleckman"
 limit 999999
 \G;
