57d168403faaeb0bb6e762c03524c26267be5095
Raphael Krut-Landau <raphael.kl@gmail.com>
2017-02-19 12:32:43 -0500

    hasty commit without summary

---
+++ is_child_in_law_of.sql
@@ -0,0 +1,21 @@
+set sql_big_selects=1;
+
+-- create table if not exists is_child_in_law_of like is_child_of;
+
+-- insert into is_child_in_law_of (person_id, person_fullname, relation_id, relation_fullname)
+select
+
+distinct
+m.person_id as person_id,
+m.person_fullname as person_fullname,
+c.relation_id as relation_id,
+c.relation_fullname as relation_fullname
+
+from has_been_married_to m
+join is_child_of c
+
+where m.relation_id = c.person_id  -- the person is married to the child of relation
+AND c.person_fullname = "Harris Gleckman"
+LIMIT 0, 999999
+
+\G;
