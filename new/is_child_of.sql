2982202a929cdba7ba1ac3d17c049c3904ad7ed6
Raphael Krut-Landau <raphael.kl@gmail.com>
2017-02-19 10:16:12 -0500

    hasty commit without summary

--- new/is_child_of.sql
+++ new/is_child_of.sql
@@ -1,20 +1,35 @@
 set sql_big_selects=1;
 
--- rename table is_child_of to is_child_of__20170219;
+-- rename table is_child_of to is_child_of__20170219_1013; -- crap version
 
-create table if not exists is_child_of like is_sibling_of;
+-- rename table is_child_of__20170219__1010 to is_child_of; -- version that was good before
 
-insert into is_child_of (person_id, person_fullname, relation_id, relation_fullname)
+-- create table if not exists is_child_of like is_sibling_of;
+
+insert ignore into is_child_of (person_id, person_fullname, relation_id, relation_fullname)
+-- select
+-- p.id as person_id,
+-- p.fullname as person_fullname,
+-- p2.id as relation_id,
+-- p2.fullname as relation_fullname
+-- from JewishMeNames as p
+-- join JewishMeNames as p2
+-- where
+-- p.id <> 0
+-- and p2.id <> 0
+-- and p2.id in (p.motherid, p.fatherid)
+-- 
+-- union
+
 select
-p.id as person_id,
-p.fullname as person_fullname,
-p2.id as relation_id,
-p2.fullname as relation_fullname
+p2.id as person_id,
+p2.fullname as person_fullname,
+p.id as relation_id,
+p.fullname as relation_fullname
 from JewishMeNames as p
+join Relationships r
+on p.id = r.PersonId
 join JewishMeNames as p2
-where
-p.id <> 0
-and p2.id <> 0
-and p2.id in (p.motherid, p.fatherid)
-limit 0,999999
+on p2.id = r.RelRecId2
+where r.Relationship_1 in ("Son","Daughter","son","daughter")
 \G;
