74ca24af00cd815751b921529bf6a78cadafc1f3
Raphael Krut-Landau <raphael.kl@gmail.com>
2017-02-19 11:51:02 -0500

    hasty commit without summary

--- new/final.sql
+++ new/final.sql
@@ -1,16 +1,21 @@
-select * from
+create table if not exists smjca_relation (
+cemetery varchar(60),
+section varchar(10),
+subsection varchar(10),
+p1_lastname varchar(255),
+p2_firstname varchar(255),
+relationship varchar(255),
+relationship_more_specific varchar(255),
+p2_lastname varchar(255),
+p2_firstname varchar(255),
+p1_id integer,
+p2_id integer)
 (
-select 'Cemetery', 'Section', 'Subsection',
-'last name', 'full first name',
-'relationship',
-'relation last name', 'relation first name',
-'person id', 'relation id'
-
-union
 
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
-'is a current or former spouse of',
+'spouse of',
+if(p.Gender like '%f%','wife of','husband of'),
 p2.lastname, p2.firstname,
 p.id, p2.id
 from has_been_married_to i
@@ -23,7 +28,8 @@ union
 
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
-'is a child of',
+'child of',
+if(p.Gender like '%f%','daughter of','son of'),
 p2.lastname, p2.firstname,
 p.id, p2.id
 from is_child_of i
@@ -36,7 +42,8 @@ union
 
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
-'is a sibling of',
+'sibling of',
+if(p.Gender like '%f%','sister of','brother of'),
 p2.lastname, p2.firstname,
 p.id, p2.id
 from is_sibling_of i
@@ -49,7 +56,8 @@ union
 
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
-'is a parent of',
+'parent of',
+if(p.Gender like '%f%','mother of','father of'),
 p2.lastname, p2.firstname,
 p.id, p2.id
 from is_child_of i
@@ -62,7 +70,8 @@ union
 
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
-'is a grandparent of',
+'grandparent of',
+if(p.Gender like '%f%','grandmother of','grandfather of'),
 p2.lastname, p2.firstname,
 p.id, p2.id
 from is_grandparent_of i
@@ -75,7 +84,8 @@ union
 
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
-'is an aunt/uncle of',
+'aunt/uncle of',
+if(p.Gender like '%f%','aunt of','uncle of'),
 p2.lastname, p2.firstname,
 p.id, p2.id
 from is_auntuncle_of i
@@ -89,6 +99,7 @@ union
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
 'is an niece/nephew of',
+if(p.Gender like '%f%','niece of','nephew of'),
 p2.lastname, p2.firstname,
 p.id, p2.id
 from is_niecenephew_of i
@@ -101,7 +112,8 @@ union
 
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
-'is a first cousin of',
+'first cousin of',
+'first cousin of',
 p2.lastname, p2.firstname,
 p.id, p2.id
 from is_firstcousin_of i
@@ -114,7 +126,8 @@ union
 
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
-'is a greataunt/greatuncle of',
+'greataunt/greatuncle of',
+if(p.Gender like '%f%','greataunt of','greatuncle of'),
 p2.lastname, p2.firstname,
 p.id, p2.id
 from is_grandniecenephew_of i
@@ -127,7 +140,8 @@ union
 
 select p.Cemetery, p.Section, p.Subsection,
 p.lastname, p.firstname,
-'is a grandniece/grandnephew of',
+'grandniece/grandnephew of',
+if(p.Gender like '%f%','grandniece of','grandnephew of'),
 p2.lastname, p2.firstname,
 p.id, p2.id
 from is_grandniecenephew_of i
@@ -136,9 +150,26 @@ join JewishMeNames p2
 where p.id = i.person_id and p.sinai
 AND p2.id = i.relation_id
 
-) as list
+);
 
+select * from
+(
+    'cemetery',
+    'section',
+    'subsection',
+    'p1_lastname',
+    'p2_firstname',
+    'relationship',
+    'relationship_more_specific',
+    'p2_lastname',
+    'p2_firstname',
+    'p1_id',
+    'p2_id'
+    union
+    select * from smjca_relation
+)
 order by 1, 2, 3, 4
 LIMIT 99999999
-into outfile '/Users/raf/code/smjca/final.csv' fields terminated by ';' enclosed by '"' lines terminated by '\n';
+into outfile '/Users/raf/code/smjca/final.csv'
+fields terminated by ';' enclosed by '"' lines terminated by '\n';
 ;
